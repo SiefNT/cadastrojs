f23d84c040b6621a4c95648ec8a9634be0e6cee4
Gabriel Leite <100167077+SiefNT@users.noreply.github.com>
2023-05-18 14:53:51 -0300

    Update cadastro.js

--- cadastro.js
+++ cadastro.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -11,90 +11,50 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Conectar-se ao banco de dados MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+MongoClient.connect(process.env.MONGO_URI, { useUnifiedTopology: true })
+  .then((client) => {
+    const db = client.db();
 
-// Verificar a conexão com o banco de dados
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Erro na conexão com o banco de dados:'));
-db.once('open', () => {
-  console.log('Conexão com o banco de dados estabelecida com sucesso!');
-});
+    // Definir o esquema do modelo de dados
+    const userCollection = db.collection('users');
 
-// Definir o esquema do modelo de dados
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  age: Number,
-});
+    // Rotas da API
+    const router = express.Router();
 
-// Criar o modelo "User" baseado no esquema
-const User = mongoose.model('User', userSchema);
-
-// Rotas da API
-const router = express.Router();
-
-// Middleware para todas as requisições
-router.use((req, res, next) => {
-  console.log('Requisição sendo processada...');
-  next();
-});
-
-// Rota de exemplo
-router.get('/', (req, res) => {
-  res.json({ message: 'Bem-vindo à API de cadastro!' });
-});
-
-// Rotas para CRUD de usuários
-router.route('/users')
-  // Criar um novo usuário
-  .post((req, res) => {
-    const user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      age: req.body.age,
+    // Middleware para todas as requisições
+    router.use((req, res, next) => {
+      console.log('Requisição sendo processada...');
+      next();
     });
 
-    user.save((err) => {
-      if (err) {
-        res.send(err);
-      } else {
-        res.json({ message: 'Usuário criado com sucesso!' });
-      }
+    // Rota de exemplo
+    router.get('/', (req, res) => {
+      res.json({ message: 'Bem-vindo à API de cadastro!' });
+    });
+
+    // Rota para criar um novo usuário
+    router.post('/users', (req, res) => {
+      const user = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      };
+
+      userCollection.insertOne(user)
+        .then(() => {
+          res.json({ message: 'Usuário criado com sucesso!' });
+        })
+        .catch((err) => {
+          res.status(500).json({ error: 'Erro ao criar o usuário.' });
+        });
+    });
+
+    // Iniciar o servidor
+    app.use('/api', router);
+    app.listen(port, () => {
+      console.log(`Servidor em execução no endereço http://localhost:${port}`);
     });
   })
-  // Obter todos os usuários
-  .get((req, res) => {
-    User.find((err, users) => {
-      if (err) {
-        res.send(err);
-      } else {
-        res.json(users);
-      }
-    });
+  .catch((err) => {
+    console.error('Erro na conexão com o banco de dados:', err);
   });
-
-// Rotas para um usuário específico
-router.route('/users/:user_id')
-  // Obter um usuário por ID
-  .get((req, res) => {
-    User.findById(req.params.user_id, (err, user) => {
-      if (err) {
-        res.send(err);
-      } else if (user) {
-        res.json(user);
-      } else {
-        res.status(404).json({ message: 'Usuário não encontrado!' });
-      }
-    });
-  });
-
-// Registrar as rotas
-app.use('/api', router);
-
-// Iniciar o servidor
-app.listen(port, () => {
-  console.log(`Servidor em execução na porta ${port}`);
-});
